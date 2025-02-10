ac36b4433d363bc80619c3ce47593f1237d22626
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2025-02-10 14:10:29 +0100

    about us animation

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,7 +4,6 @@
 import Link from "next/link";
 
 import Image from "next/image";
-import { useRouter } from "next/router";
 import LogoBlackImg from "../assets/images/sec_logo.svg";
 // import MenuSVG from "../assets/images/menu.svg";
 // import LogoWhiteImg from "../assets/images/logo-white.svg";
